d8bb7ab29d707accec0e1a101f9b1c15fbada1e6
progriro <progriro@gmail.com>
2020-07-11 20:59:13 +0900

    [abc147]b

---
+++ abc147/b/main.cpp
@@ -0,0 +1,15 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+// #define PI acos(-1.0)
+#define PI 3.14159265358979323846264338327950L
+#define FOR(I, A, B) for (int I = (A); I < (B); ++I)
+
+int main() {
+  int a1, a2, a3;
+  cin >> a1 >> a2 >> a3;
+
+  cout << (a1 + a2 + a3 >= 22 ? "bust" : "win") << endl;
+}
