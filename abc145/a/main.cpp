4c45a1400e64459e4113b76686d35eebd46a981d
progriro <progriro@gmail.com>
2020-07-28 14:05:37 +0900

    [abc145]ab

---
+++ abc145/a/main.cpp
@@ -0,0 +1,10 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define PI acos(-1.0);
+
+int main() {
+  int r;
+  cin >> r;
+  cout << r * r << endl;
+}
---
+++ abc145/b/main.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define PI acos(-1.0);
+
+int main() {
+  int n, cnt = 0;
+  string s;
+  cin >> n >> s;
+  for (int i = 0; i < n / 2; ++i)
+    if (s[i] == s[i + n / 2]) cnt++;
+  if (n % 2 == 0)
+    cout << ((cnt == n / 2) ? "Yes" : "No") << endl;
+  else
+    cout << "No" << endl;
+}
